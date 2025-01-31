6d62d3123701625b5871f0f3f20164951c9ef694
Inaki Vazquez <ivazquez@deusto.es>
2025-01-31 23:32:20 +0100

    random target

--- mujobot/envs/ur5_gripper_v0.py
+++ mujobot/envs/ur5_gripper_v0.py
@@ -11,6 +11,7 @@ from robot_descriptions import ur5e_mj_description
 import os
 
 
+
 class UR5GripperEnv(gym.Env):
 
     metadata = {'render_modes': ['human'], 'render_fps': 30}
@@ -29,11 +30,12 @@ class UR5GripperEnv(gym.Env):
             self.viewer = mujoco.viewer.launch_passive(self.model, self.data)
 
         # Target pos, current pos of ee, joint positions
-        self.observation_space = gym.spaces.Box(low=np.array([-1]*6 + [-2*math.pi]*6, dtype=np.float32), high=np.array([+1]*6 + [+2*math.pi]*6, dtype=np.float32), shape=(12,))
+        self.observation_space = gym.spaces.Box(low=np.array([-1]*3 + [-2*math.pi]*6, dtype=np.float32), high=np.array([+1]*3 + [+2*math.pi]*6, dtype=np.float32), shape=(9,))
 
         # Action space is 6 joint angles and the gripper open/close level (0 to 1)
         action_max = 2*math.pi / 10
         self.action_space = gym.spaces.Box(low=np.array([-action_max]*6+[0]), high=np.array([+action_max]*6+[1]), shape=(7,))
+        self.target_id = mujoco.mj_name2id(self.model, mujoco.mjtObj.mjOBJ_SITE, "target")
 
 
     def reset(self, *, seed: int | None = None, options: dict[str, Any] | None = None) -> tuple[ObsType, dict[str, Any]]:
@@ -41,8 +43,15 @@ class UR5GripperEnv(gym.Env):
         mujoco.mj_resetData(self.model, self.data)  # Fully resets simulation state
         self.target_pos = np.random.uniform(-0.5, 0.5, size=3)
         self.target_pos[2] = 0.7
-        if self.render_mode == "human":
-            self.draw_sphere(self.target_pos, 0.025)
+        self.model.body_pos[self.target_id] = self.target_pos  # Update position
+
+        qpos_addr = self.model.jnt_qposadr[mujoco.mj_name2id(self.model, mujoco.mjtObj.mjOBJ_JOINT, "target_joint")]
+
+        self.data.qpos[qpos_addr:qpos_addr+3] = self.target_pos  # Teleport
+
+
+        #if self.render_mode == "human":
+            #self.draw_sphere(self.target_pos, 0.025)
         return self.get_observation(), {}
 
     def wait_until_stable(self, sim_steps=500):
@@ -61,15 +70,17 @@ class UR5GripperEnv(gym.Env):
     def step(self, action: ActType) -> tuple[ObsType, SupportsFloat, bool, bool, dict[str, Any]]:
         assert self.action_space.contains(action), f"Invalid Action: {action}"
 
+        action[0:6] = self.data.ctrl[0:6] + action[0:6]
         action[6] = 0 if action[6] < 0.5 else 255
 
-        self.data.ctrl[:] += action
+        self.data.ctrl[:] = action
         self.data.ctrl[:] = np.clip(self.data.ctrl, -2*math.pi, +2*math.pi)
         self.wait_until_stable()
 
+        self.update_target_pos()
+
         obs = self.get_observation()
-        ee_pos = obs[3:6]
-        distance = np.linalg.norm(self.target_pos - ee_pos)
+        distance = np.linalg.norm(obs[0:3])
         reward = -distance
         terminated = (distance < 0.05)
         if terminated:
@@ -88,12 +99,22 @@ class UR5GripperEnv(gym.Env):
     def get_observation(self):
         ee_pos, ee_quat = self.get_ee_pose()
         joint_pos = self.data.qpos[0:6]
-        return np.concatenate((self.target_pos, ee_pos, joint_pos), dtype=np.float32)
+        distance_vector = (self.target_pos - ee_pos)
+        return np.concatenate((distance_vector, joint_pos), dtype=np.float32)
 
     def get_ee_pose(self):
-        link_name = "pinch"
-        link_id = mujoco.mj_name2id(self.model, mujoco.mjtObj.mjOBJ_BODY, link_name)
-        return self.data.xpos[link_id], self.data.xquat[link_id]
+        pinch_name = "pinch"
+        pinch_id = mujoco.mj_name2id(self.model, mujoco.mjtObj.mjOBJ_BODY, pinch_name)
+        ee_pos_absolute = self.data.site_xpos[pinch_id]
+        rotation_matrix = self.data.xmat[pinch_id].reshape(3, 3)  # Extract 3×3 rotation matrix
+        quat = [0]*4  # Convert to quaternion (x, y, z, w)
+
+        # site_* methods to access global coordinates, other * methods (xpos, xquat) access local coordinates
+        return ee_pos_absolute, quat
+    
+    def update_target_pos(self):
+        self.target_pos = self.model.body_pos[self.target_id]
+        return self.target_pos
     
     def draw_sphere(self, center, radius, color=(1, 0, 0, 1)):
         """Draws a debug sphere at a given 3D position in MuJoCo."""
